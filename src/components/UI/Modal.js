5b5e7821b608b4aee98c7b2dfee50cc03940b84d
Yunus Emre Aksu <emre_aksu@live.com>
2022-03-15 10:36:50 +0300

    Managed Cart and Modal State

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -15,13 +15,19 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays")
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
-      {ReactDOM.createPortal(<ModalOverlay> {props.children} </ModalOverlay>, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay> {props.children} </ModalOverlay>,
+        portalElement
+      )}
     </React.Fragment>
   );
 };
